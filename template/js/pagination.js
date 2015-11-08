390a6cbb9d827e1643e837fdd7190f5e1f1a3425
gui machiavelli <hey@guimachiavelli.com>
2015-11-08 15:23:18 +0100

    pagination: add counter, new copy

--- template/js/pagination.js
+++ template/js/pagination.js
@@ -9,17 +9,32 @@
     pagination = {
         nextButton: null,
         prevButton: null,
+        paginationCounter: null,
+        paginationCurrent: null,
+        total: 0,
+        current: 0,
 
         init: function(el) {
+            var pNumber;
+
             if (el === null) {
                 return;
             }
 
             fontStretch.init(el);
 
-            if (el.querySelectorAll('p').length < 2) {
+            pNumber = el.querySelectorAll('p').length;
+
+            if (pNumber < 2) {
                 return;
             }
+
+            this.setup(el, pNumber);
+        },
+
+        setup: function(el, pNumber) {
+            this.total = pNumber;
+            this.current = 1;
 
             this.addButtons(el);
             this.bind(el);
@@ -40,15 +55,20 @@
         },
 
         addButtons: function(el) {
-            var next, previous, fragment;
+            var fragment;
 
             this.previousButton = this.button('previous');
             this.previousButton.disabled = true;
             this.nextButton = this.button('next');
+            this.paginationCounter = this.counter();
+            this.paginationCurrent = this.paginationCounter
+                                .querySelector('.pagination-counter__current');
+
 
             fragment = document.createDocumentFragment();
             fragment.appendChild(this.previousButton);
             fragment.appendChild(this.nextButton);
+            fragment.appendChild(this.paginationCounter);
 
             el.parentNode.appendChild(fragment);
         },
@@ -58,9 +78,29 @@
 
             btn = document.createElement('button');
             btn.className = 'pagination-button--' + type;
-            btn.innerHTML = type + ' page';
+            btn.innerHTML = type === 'next' ? 'Continue reading' : 'Previous';
 
             return btn;
+        },
+
+        counter: function() {
+            var counter, current, total;
+
+            counter = document.createElement('div');
+            current = document.createElement('span');
+            total = document.createElement('span');
+
+            total.innerHTML = '/' + this.total;
+            total.className = 'pagination-counter__total';
+
+            current.innerHTML = this.current;
+            current.className = 'pagination-counter__current';
+
+            counter.appendChild(current);
+            counter.appendChild(total);
+            counter.className = 'pagination-counter';
+
+            return counter;
         },
 
         advance: function(el, e) {
@@ -72,13 +112,14 @@
             next = helpers.nextSiblingOfType(target, 'P');
             previous = helpers.previousSiblingOfType(next, 'P');
 
-
             if (next === null) {
                 return;
             }
 
             next.classList.add('carousel--active');
             target.classList.remove('carousel--active');
+
+            this.updateCounter(1);
             this.updateButtons(helpers.nextSiblingOfType(next, 'P'), previous);
         },
 
@@ -98,12 +139,19 @@
 
             previous.classList.add('carousel--active');
             target.classList.remove('carousel--active');
+
+            this.updateCounter(-1);
             this.updateButtons(next, helpers.previousSiblingOfType(previous, 'P'));
         },
 
         updateButtons: function(next, prev) {
             this.nextButton.disabled = !next;
             this.previousButton.disabled = !prev;
+        },
+
+        updateCounter: function(increment) {
+            this.current += increment;
+            this.paginationCurrent.innerHTML = this.current;
         }
 
     };
